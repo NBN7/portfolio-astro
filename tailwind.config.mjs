5873298a5668c253bee06a2093ae7682a1fcdaf1
Ignacio Biran <nqchossj@gmail.com>
2024-01-12 23:12:18 -0300

    add primary color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,7 +7,11 @@ export default {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        primary: "#0070F0",
+      },
+    },
   },
   darkMode: "class",
   plugins: [
