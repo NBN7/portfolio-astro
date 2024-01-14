94e670d90e63fa759deda21a3e3af67150c319d9
Ignacio Biran <nqchossj@gmail.com>
2024-01-14 03:48:38 -0300

    add projects constant

---
+++ src/constants/projects.ts
@@ -0,0 +1,10 @@
+export const PROJECTS = {
+  PASSWORD_GENERATOR: {
+    TITLE: "Password Generator",
+    DESCRIPTION:
+      "Designed to enhance online security by providing strong and unique passwords.",
+    IMAGE: "/password-generator.webp",
+    DEPLOY: "https://labo4-passwordgenerator.netlify.app/",
+    GITHUB: "https://github.com/NBN7/password-generator",
+  },
+};
