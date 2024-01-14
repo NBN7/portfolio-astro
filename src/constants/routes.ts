cefd4a76b3dcb92288518488d59a75442b97cbb9
Ignacio Biran <nqchossj@gmail.com>
2024-01-13 23:35:43 -0300

    update route constant

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,6 +1,22 @@
 export const ROUTES = {
-  HOME: "#",
-  SKILLS: "#skills",
-  PROJECTS: "#projects",
-  CONTACT: "#contact",
+  HOME: {
+    NAME: "Home",
+    PATH: "#home",
+  },
+  EXPERIENCE: {
+    NAME: "Experience",
+    PATH: "#experience",
+  },
+  SKILLS: {
+    NAME: "Skills",
+    PATH: "#skills",
+  },
+  PROJECTS: {
+    NAME: "Projects",
+    PATH: "#projects",
+  },
+  CONTACT: {
+    NAME: "Contact",
+    PATH: "#contact",
+  },
 };
