c82322f7f76955e5bbde18690a60b7d3a614408d
Ignacio Biran <nqchossj@gmail.com>
2024-01-13 23:36:40 -0300

    add experience constant

---
+++ src/constants/experience.ts
@@ -0,0 +1,14 @@
+export const EXPERIENCE = [
+  {
+    TIME: "Actually",
+    TITLE: "Fullstack Developer - Sturges Labs LLC",
+    DESCRIPTION:
+      "Fullstack developer for Sturges Labs LLC. For the Frontend, I used TypeScript, NextJS, and MUI. For the Backend, I employed TypeScript, tRPC, Clerk, MySQL and Prisma.",
+  },
+  {
+    TIME: "January 2024",
+    TITLE: "Frontend Developer - Sturges Labs LLC",
+    DESCRIPTION:
+      "Frontend developer for Sturges Labs LLC. I used TypeScript, NextJS, and MUI.",
+  },
+];
