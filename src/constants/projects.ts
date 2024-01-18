1d2975ff20e879a7d7217a3cdf97dfc58b39e509
Ignacio Biran <nqchossj@gmail.com>
2024-01-18 20:59:40 -0300

    update projects constant

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,18 +1,16 @@
-export const PROJECTS = {
-  PASSWORD_GENERATOR: {
+export const PROJECTS = [
+  {
     TITLE: "Password Generator",
-    DESCRIPTION:
-      "Designed to enhance online security by providing strong and unique passwords.",
-    IMAGE: "/password-generator.webp",
+    DESCRIPTION: "Security above all",
+    IMAGE: "/password-generator.png",
     DEPLOY: "https://labo4-passwordgenerator.netlify.app/",
     GITHUB: "https://github.com/NBN7/password-generator",
   },
-  CHESS_TIMER: {
+  {
     TITLE: "Chess Timer",
-    DESCRIPTION:
-      "This application offers intuitive and customizable features that elevate the level of comfort and flexibility during chess matches.",
-    IMAGE: "/chess-timer.webp",
+    DESCRIPTION: "The time is running",
+    IMAGE: "/chess-timer.png",
     DEPLOY: "https://labo4-chesstimer.netlify.app/",
     GITHUB: "https://github.com/NBN7/chess-timer",
   },
-};
+];
