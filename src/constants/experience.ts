8ba3d0a629efa72f9f675145d781a1cf55553a45
Ignacio Biran <nqchossj@gmail.com>
2024-01-18 23:07:48 -0300

    add gradient to text

--- src/constants/experience.ts
+++ src/constants/experience.ts
@@ -1,13 +1,13 @@
 export const EXPERIENCE = [
   {
     TIME: "Actually",
-    TITLE: "Fullstack Developer - Sturges Labs LLC",
+    TITLE: "Fullstack Dev - Sturges Labs LLC",
     DESCRIPTION:
       "Fullstack developer for Sturges Labs LLC. For the Frontend, I use TypeScript, NextJS, and MUI. For the Backend, I employ TypeScript, tRPC, Clerk, MySQL and Prisma.",
   },
   {
     TIME: "January 2024",
-    TITLE: "Frontend Developer - Sturges Labs LLC",
+    TITLE: "Frontend Dev - Sturges Labs LLC",
     DESCRIPTION:
       "Frontend developer for Sturges Labs LLC. I used TypeScript, NextJS and MUI.",
   },
