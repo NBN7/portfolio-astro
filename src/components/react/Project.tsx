d6dfea4d980cfd980eb2f82004b7c65413f2510e
Ignacio Biran <nqchossj@gmail.com>
2024-01-18 23:36:38 -0300

    add info popover

--- src/components/react/Project.tsx
+++ src/components/react/Project.tsx
@@ -1,7 +1,17 @@
-import { Card, CardHeader, CardFooter, Button, Link } from "@nextui-org/react";
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  Button,
+  Link,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@nextui-org/react";
 
 import { FaGithub } from "react-icons/fa";
 import { FaBolt } from "react-icons/fa";
+import { IoIosInformationCircleOutline } from "react-icons/io";
 
 interface ProjectProps {
   description: string;
@@ -18,8 +28,27 @@ export const Project = ({
   github,
   deploy,
 }: ProjectProps) => {
+  const iconSize = "20px";
+
   return (
     <Card className="col-span-12 sm:col-span-4 h-[300px]">
+      <div className="absolute z-50 top-0 right-0 flex items-center justify-center w-12 h-12 ">
+        <Popover placement="top" backdrop="opaque">
+          <PopoverTrigger>
+            <Button className="bg-transparent" isIconOnly radius="full">
+              <IoIosInformationCircleOutline size={iconSize} />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent>
+            <div className="px-1 py-2">
+              <h4 className="text-small font-bold">Technologies</h4>
+              <p className="text-tiny">
+                React.js, TypeScript, Tailwind CSS, Vite.js
+              </p>
+            </div>
+          </PopoverContent>
+        </Popover>
+      </div>
       <CardHeader className="absoulte z-10 top-1 flex-col !items-start">
         <p className="text-tiny text-white/60 uppercase font-bold">
           {description}
@@ -30,13 +59,13 @@ export const Project = ({
       <CardFooter className="flex absolute bg-black/40 bottom-0 z-10 border-t-1 border-white/15">
         <Link href={github} target="_blank">
           <Button className="bg-transparent" isIconOnly radius="full">
-            <FaGithub size="20px" />
+            <FaGithub size={iconSize} />
           </Button>
         </Link>
 
         <Link href={deploy} target="_blank">
           <Button className="bg-transparent" isIconOnly radius="full">
-            <FaBolt size="20px" />
+            <FaBolt size={iconSize} />
           </Button>
         </Link>
       </CardFooter>
